5febd4b02609f6df876f0f1e4aff04a337773af1
Yuriy Zhmurko <777zhm@gmail.com>
2018-12-27 19:48:14 +0200

    backtotop fadeOut repaired

--- js/backtotop.js
+++ js/backtotop.js
@@ -3,27 +3,17 @@ var scrolled = window.pageYOffset || document.documentElement.scrollTop; // сч
 
 if(scrolled > 10){
   $('.backtotop_button').fadeIn(500); // когда проскоблили больше 10 пикселей - кнопка появляется за полсекунды (500 миллисекунд)
+  $('header').addClass('menu_fixed');
+
 } else{
   $('.backtotop_button').fadeOut(500); // когда проскоблили вверх - кнопка исчезает за полсекунды (500 миллисекунд)
-
+  $('header').removeClass('menu_fixed');
 }
 
 }
+
       
 $('.backtotop_button').click(function(){ 
         $('html,body').animate({ scrollTop: 0 } );
         return false; 
 });
-
-
-window.onscroll = function() { //вызываем при скролле
-var scrolled = window.pageYOffset || document.documentElement.scrollTop; // считаем, сколько проскролили
-
-if(scrolled > 10){
-  $('header').addClass('menu_fixed'); // когда проскоблили больше 10 пикселей - кнопка появляется за полсекунды (500 миллисекунд)
-} else{
-  $('header').removeClass('menu_fixed'); // когда проскоблили вверх - кнопка исчезает за полсекунды (500 миллисекунд)
-
-}
-
-}
