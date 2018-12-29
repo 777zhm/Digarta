0cf4d97be02738bc899408041784dbf27987d3a7
Yuriy Zhmurko <777zhm@gmail.com>
2018-12-29 11:38:53 +0200

    owl event listener

--- js/owl.carousel_config.js
+++ js/owl.carousel_config.js
@@ -1,9 +1,22 @@
 $(document).ready(function(){
-        $('.owl-carousel').owlCarousel({
+var owl = $('.owl-carousel');
+var target = $('.casestudy_slide_bg')
+
+owl.owlCarousel({
     loop:true,
     margin:10,
     items:1
 })
+
+owl.on('changed.owl.carousel', function(event) {
+	
+   	target.addClass('test');
+   
+   // target.css('background-image', 'url("' + img/slider_bg_2.jpg + '")');
+})
+
+
+
 });
 
 
